a33a06d476a970dde5bb2bd8745118e16855ef8c
faroke <yann.fo7@gmail.com>
2022-03-14 12:02:16 +0100

    ex8

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 function UserGreeting(props) {
-    return <h1>Bienvenue !</h1>;
+    return <div>
+        <h1>Bienvenue !</h1>
+        <DisplayArray/>
+    </div>;
   }
   
   function GuestGreeting(props) {
     return <h1>Veuillez vous connecter</h1>;
   }
+
+  function DisplayArray(){
+      const [data, setData] = React.useState([]);
+      React.useEffect(() => {
+          fetch('https://jsonplaceholder.typicode.com/users')
+              .then((response) => response.json())
+              .then((data) => {
+                  setData(data);
+              })
+      }, [])
+      const listItems = data.map((item, index) => <li key={index}>{`${item.name} ; ${item.email}, ${item.company.name} ; ${item.phone} ; ${item.website}`}</li>)
+      return <ul>{listItems}</ul>
+  }
+
+
   function Greeting(props) {
       const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
