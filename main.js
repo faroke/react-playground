6805e350bed824042224d20b2eceaf42922135c2
faroke <yann.fo7@gmail.com>
2022-03-14 11:23:24 +0100

    ex4

--- main.js
+++ main.js
@@ -3,9 +3,20 @@
 const firstName = 'r0ulito';
 const lastName = 'formateur';
 
+function Welcome({f, l}) {
+        return <h1>Bonjour, {}</h1>;
+}
 
-// Solution sans bonus
-const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
+function toUpper(text){
+        return <span className="red-text">{text.toUpperCase()}</span>
+}
+function fistToUpper(text){
+        return <span className="red-text">{text[0].toUpperCase() + text.slice(1)}</span>
+}
+
+function Main(){
+        return <div>{toUpper(lastName)} {fistToUpper(firstName)}</div>
+}
 
 
 
@@ -29,4 +40,4 @@ const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice
 
  */
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+ReactDOM.render(<Main/>, document.querySelector('#app'));
