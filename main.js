87d1d54a104866f60f4f753324839fb105d36f59
faroke <yann.fo7@gmail.com>
2022-03-14 11:15:30 +0100

    ex1

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
-
+const lastName = 'faussette';
+const firstName = 'yann';
 const helloWorld = <h1>Hello world!</h1>;
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+function returnToUp(f, l) {
+        return `${l} ${f}`.toUpperCase()
+}
+
+const displayName = <span className="maclass">{returnToUp(firstName, lastName)}</span>;
+
+//ReactDOM.render(helloWorld, document.querySelector('#app'));
+ReactDOM.render(displayName, document.querySelector('#app'));
