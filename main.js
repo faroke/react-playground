df425688d298615ed2998a111686c15dda7a8156
faroke <yann.fo7@gmail.com>
2022-03-14 12:12:01 +0100

    ex9

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+function NameForm(props){
+    const [value, setValue] = React.useState('');
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        alert(`Le nom a été soumis ${value}`)
+        e.preventDefault();
+    }
+    return <form onSubmit={handleSubmit}>
+        <label>
+            Nom :
+            <input type="text" value={value} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Envoyer" />
+    </form>
+}
+function EssayForm(props){
+    const [value, setValue] = React.useState('Écrivez un essai à propos de votre élément du DOM préféré');
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        alert(`Un essai a été envoyé : ${value}`);
+        e.preventDefault();
+    }
+    return <form onSubmit={handleSubmit}>
+        <label>
+            Essay:
+            <textarea value={value} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Envoyer" />
+    </form>
+}
+function FlavorForm(props){
+    const [value, setValue] = React.useState('coconut');
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        alert(`Un essai a été envoyé : ${value}`);
+        e.preventDefault();
+    }
+    return <form onSubmit={handleSubmit}>
+        <label>
+            Choisissez votre parfum favori :
+            <select value={value} onChange={handleChange}>
+                <option value="grapefruit">Pamplemousse</option>
+                <option value="lime">Citron vert</option>
+                <option value="coconut">Noix de coco</option>
+                <option value="mango">Mangue</option>
+            </select>
+        </label>
+        <input type="submit" value="Envoyer" />
+    </form>
+}
 function App(props) {
     React.useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
